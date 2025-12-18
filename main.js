7e22a6c0548d2ac4edeb56d6b41ca15f2b8743b5
minoche95 <minoche95@gmail.com>
2025-12-18 14:01:35 +0100

    v3.1
    Ajout de commentaires au code

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const button = document.getElementById('light')
-const body = document.body
+const button = document.getElementById('light')  // Attribution de l'id du bouton pour le light mode 
+const light = document.body // Creation de la variable 'light' pour definir le mode light dans le CSS 
 
-button.addEventListener('click', function() {
-    body.classList.toggle('light')
+button.addEventListener('click', function() { // Définition de la fonction du bouton pour activer/désactiver le mode light au click
+    light.classList.toggle('light')
 })
