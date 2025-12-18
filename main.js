89a2e26be57faadea7e67abaa26828b18cf249d4
minoche95 <minoche95@gmail.com>
2025-12-18 23:32:37 +0100

    Scroll menu v1
    Ajout du scroll menu, modification du bouton light/dark mode

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-const button = document.getElementById('light')  // Attribution de l'id du bouton pour le light mode 
-const light = document.body // Creation de la variable 'light' pour definir le mode light dans le CSS 
+const lightbutton = document.getElementById('light')  // Attribution de l'id du bouton pour le light mode 
+const scrollbutton = document.getElementById('scrollbutton')
+const light = document.body // Creation de la variable 'light' pour definir le mode light dans le CSS
+const scrollmenu = document.getElementById('scroll')
 
-button.addEventListener('click', function() { // Définition de la fonction du bouton pour activer/désactiver le mode light au click
+
+lightbutton.addEventListener('click', function() { // Définition de la fonction du bouton pour activer/désactiver le mode light au click
     light.classList.toggle('light')
+})
+
+scrollbutton.addEventListener('click', function() { // Définition de la fonction pour affiché le menu déroulant en activant '.show' dans le css 
+    scrollmenu.classList.toggle('show')
 })
